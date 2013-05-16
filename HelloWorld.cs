abfe7fc7a75478923f216092e0cea9af3b8e5d38
Stephen Walentik <swalenti@kent.edu>
2013-05-16 01:39:14 -0400

    change HW.cs, installed mono, compiled in exe and works

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -1,5 +1,11 @@
 public class HelloWorld{
     public static void Main(){
         System.Console.WriteLine("Hello World");
+        System.Console.WriteLine("My first .EXE");
+        System.Console.WriteLine("Passcode: \n>");
+	string line = System.Console.ReadLine();
+	if(line == "Paragon"){
+	    System.Console.WriteLine("Good work, it compile sir!");
+	}
     }
 }
