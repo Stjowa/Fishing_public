5d27d1560407bf6d23e2d05cb16917f74c645469
Stephen Walentik <swalenti@kent.edu>
2013-05-17 01:43:33 -0400

    Work on the SDL library

---
+++ exampleSDL.cpp
@@ -0,0 +1,24 @@
+#include "SDL/SDL.h"
+
+int main(int argc, char* args[]){
+    SDL_Surface* background=NULL;                                       //Image
+    SDL_Surface* screen=NULL;                                           //Image
+
+    SDL_Init(SDL_INIT_EVERYTHING);                                      //Starts SDL
+
+    screen = SDL_SetVideoMode(640, 480, 32, SDL_SWSURFACE);             //Sets up the screen
+
+    background = SDL_LoadBMP("background.jpg");                         //hello is the image hello.bmp
+
+    SDL_BlitSurface(background, NULL, screen, NULL);                    //This applies the image to the screen
+
+    SDL_Flip(screen);                                                   //Updates screen
+
+    SDL_Delay(2000);                                                    //Pause
+
+    SDL_FreeSurface(background);                                        //Frees the loaded image
+
+    SDL_Quit();                                                         //Quit SDL
+
+    return 0;
+}
